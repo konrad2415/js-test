15966d9b53fca2f0c63d0cf276ef53fcbf7bdc4f
konradzuse.dm@gmail.com <konradzuse.dm@gmail.com>
2023-07-10 11:36:07 -0400

    using lastIndexOf()

--- first-test/js/app.js
+++ first-test/js/app.js
@@ -284,4 +284,9 @@ l3.824-2.601l0.27,0.152c0.849,0.479,1.782,0.863,2.773,1.144l0.297,0.084L17.734,3
               console.log("String charCodeAt() 2 -"+text.charCodeAt(2));
               let newArray = text.split(" ",4);
               console.log("Using the split(3) at 4rth elemento of the retuned array: "+newArray[3]);
-              console.log("Using the indexOf() :"text.indexOf("something"));
+              
+              // Using indexOf() lastIndexOf() 
+              console.log("Using the indexOf() :"+text.indexOf("something"));
+              
+              text = "Please locate where 'locate' occurs!";
+              console.log("Last ocurrence of locate is at: "+text.lastIndexOf("locate"));
